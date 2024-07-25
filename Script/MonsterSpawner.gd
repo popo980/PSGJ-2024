0f693e9407bfe080d7e57f14f25e141bc8f2b612
popoulpe <54855619+popo980@users.noreply.github.com>
2024-07-25 18:56:09 +0200

    installation monster spawner

---
+++ Script/MonsterSpawner.gd
@@ -0,0 +1,31 @@
+extends Marker2D
+
+enum ListMonster {
+	NIGHTMORPH
+}
+var Monsters = {
+	ListMonster.NIGHTMORPH : preload("res://Scene/nightmorph.tscn")
+}
+
+@onready var spawn_timer = $SpawnTimer
+@export var choosenMonster :ListMonster= ListMonster.NIGHTMORPH
+@export var spawnCooldown :float= 1
+@export var qttSpawned :int = 1
+
+
+func _ready():
+	spawn_timer.wait_time = spawnCooldown
+	
+	#A supprimer sinon spawner useless
+	AtNight()
+
+func AtNight():
+	spawn_timer.start()
+
+
+func _on_spawn_timer_timeout():
+	if qttSpawned <=1:
+		spawn_timer.stop()
+	qttSpawned -=1
+	var spawned = Monsters[choosenMonster].instantiate()
+	add_child(spawned)
---
+++ Script/Nightmorph.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+@export var speed := 100.0
+
+var player
+
+func _ready():
+	player = get_parent().get_parent().get_parent().get_node("Player")
+
+func _physics_process(_delta):
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * speed
+	move_and_slide()
+
